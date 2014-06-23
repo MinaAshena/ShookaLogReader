e6215be060aa51ca5de9577d722ec61ded08cd8c
Matin Lotfaliee <matinlotfali@gmail.com>
2014-06-23 09:17:14 +0430

    Initial Commit - v1.0.1.0

---
+++ LogForm.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+using System.Data;
+using System.IO;
+
+namespace ShookaLogReader
+{
+    public enum LogSender
+    {
+        VidyoDLL, VidyoPortal, ShookaClient, GUI, ShookaWebService
+    }
+    public enum LogType
+    {
+        Info, Warning, Error, CriticalError
+    }
+    public enum LoggedFor
+    {
+        Programmer, Maintainer
+    }
+
+    public partial class LogForm : Form
+    {
+        public LogForm()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.Add(LoggedFor.Programmer.ToString());
+            comboBox1.Items.Add(LoggedFor.Maintainer.ToString());
+            comboBox1.SelectedIndex = 2;
+
+            Text += " - نسخه " + Application.ProductVersion;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (File.Exists(openFileDialog1.FileName))
+                OpenXML();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                OpenXML();
+            }
+        }
+
+        private void OpenXML()
+        {
+            string line = "";
+            using (StreamReader reader = new StreamReader(openFileDialog1.FileName, Encoding.UTF8))
+            {
+                while (!reader.EndOfStream)
+                    line = reader.ReadLine();
+            }
+            if (line != "</LogFile>")
+            {
+                using (StreamWriter writer = new StreamWriter(openFileDialog1.FileName, true))
+                    writer.WriteLine("</LogFile>");
+                MessageBox.Show("این لاگ به صورت کامل پایان نیافته است.\nبه صورت خودکار لاگ اصلاح خواهد شد.");
+            }
+
+
+            Stream input = null;
+            if ((input = openFileDialog1.OpenFile()) != null)
+            {
+                using (input)
+                {
+                    XmlReaderSettings settings = new XmlReaderSettings();
+                    settings.CheckCharacters = false;
+                    XmlReader xmlFile = XmlReader.Create(input, settings);
+                    DataSet dataSet = new DataSet();
+                    dataSet.ReadXml(xmlFile);
+                    xmlFile.Close();
+
+                    for (int i = 0; i < dataSet.Tables[0].Rows.Count; i++)
+                    {
+                        string s = dataSet.Tables[0].Rows[i]["loggedFor"].ToString();
+                        if (!(comboBox1.SelectedIndex == 0 || comboBox1.Text == s))
+                        {
+                            dataSet.Tables[0].Rows[i].Delete();
+                            i--;
+                        }
+                    }
+
+                    dataGridView1.DataSource = dataSet.Tables[0];
+                }
+
+                dataGridView1.Columns[0].Width = 100;
+                dataGridView1.Columns[1].Width = 100;
+                dataGridView1.Columns[2].Width = 50;
+                dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dataGridView1.Columns[4].Width = 100;
+
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[i];
+
+                    string loggedFor = row.Cells["loggedFor"].Value.ToString();
+                    string type = row.Cells["type"].Value.ToString();
+
+                    string message = row.Cells["message"].Value.ToString();
+                    int n = message.Split('\n').Length;
+                    if (n > 1)
+                        row.Height *= n;
+
+
+                    if (loggedFor == LoggedFor.Maintainer.ToString())
+                    {
+                        row.DefaultCellStyle.Font = new Font(Font, FontStyle.Bold);
+                        row.DefaultCellStyle.BackColor = Color.Khaki;
+                    }
+
+                    switch (type.ToString())
+                    {
+                        case "Warning":
+                            row.DefaultCellStyle.BackColor = Color.Yellow;
+                            break;
+                        case "Error":
+                            row.DefaultCellStyle.BackColor = Color.Red;
+                            row.DefaultCellStyle.ForeColor = Color.White;
+                            break;
+                        case "CriticalError":
+                            row.DefaultCellStyle.BackColor = Color.DarkRed;
+                            row.DefaultCellStyle.ForeColor = Color.White;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            openFileDialog1.FileName = files[0];
+            OpenXML();
+        }
+
+        private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void LogForm_Load(object sender, EventArgs e)
+        {
+            string[] files = Environment.GetCommandLineArgs();
+            if (files.Length > 1)
+            {
+                openFileDialog1.FileName = files[1];
+                OpenXML();
+            }
+        }
+    }
+}
---
+++ LogForm.designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ShookaLogReader
+{
+    partial class LogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.openButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(807, 446);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.openButton);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(807, 34);
+            this.panel1.TabIndex = 1;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 5);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 1;
+            this.openButton.Text = "باز کردن لاگ";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "All"});
+            this.comboBox1.Location = new System.Drawing.Point(159, 7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter: ";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Shooka Log Files|*.logxml";
+            // 
+            // LogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 480);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "LogForm";
+            this.Text = "نمایش لاگ ها";
+            this.Load += new System.EventHandler(this.LogForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
